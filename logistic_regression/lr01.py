cc11588ba727f699464edf8169005ca757e961e1
aoliong <lyx234@gmail.com>
2018-06-23 17:36:03 +0800

    逻辑回归第一个版本

---
+++ logistic_regression/lr01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#coding=utf8
+
+#author:aoliong
+#email:lyx234@gmail.com
+
+'''
+第一个版本，逻辑回归，使用for loop.
+
+输入：X，为列向量组成的矩阵。
+Y，为列微量。
+'''
+
+def logistic_regression(X, Y):
+    '''实现逻辑回归算法,
+    返回：每个属性的权重，以及一个常数值。'''
+
